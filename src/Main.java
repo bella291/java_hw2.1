c89e316b2ebb08f6a8793870d8087d8e403762f0
Bella Khamchieva <bella.kh29@gmail.com>
2021-09-13 18:51:26 +0300

    Main.java added

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 20364;
+        int miles = ticketPrice / 20;
+        System.out.println("За покупку билета на сумму" + " " + ticketPrice + " " + "руб. вам начислено" + " " + miles + " " + "миль!");
+    }
+}
+
